71250a68e5e5a75a8a06ae27871d787c9e435c3e
Manuel M <mamueluth@gmail.com>
2023-04-27 14:27:11 +0200

    two displayed one moving

--- two_distributed_kukas/two_distributed_rrbots_bringup/launch/all_in_one.launch.py
+++ two_distributed_kukas/two_distributed_rrbots_bringup/launch/all_in_one.launch.py
@@ -26,7 +26,33 @@ from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 
+def delete_direct_slash_duplicate(t):
+    if not (t[0] == "/" and t[1] == "/"):
+        return t[0]
+
+
+def prepend_slash_if_not_null(prefix):
+    if not prefix:
+        return ""
+    ns = "/" + prefix
+    # remove all occurrences of slashes that directly follow each other ("//Prefix/////Namespace//" -> "/Prefix/Namespace/")
+    return "".join(
+        filter(
+            lambda item: item is not None,
+            map(delete_direct_slash_duplicate, zip(ns, ns[1:] + " ")),
+        )
+    )
+
+
 def generate_launch_description():
+    controller_manager_name = "controller_manager"
+    satellite_2_ns_name = "sub_2"
+    slash_controller_manager_name = prepend_slash_if_not_null(controller_manager_name)
+    slash_satellite_2_ns_name = prepend_slash_if_not_null(satellite_2_ns_name)
+    satellite_2_controller_manager_name = (
+        slash_satellite_2_ns_name + slash_controller_manager_name
+    )
+
     declared_arguments = []
 
     declared_arguments.append(
@@ -214,8 +240,112 @@ def generate_launch_description():
         )
     )
 
+    # SUB 2
+    robot_satellite_2_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("kuka_ros2_control_support"),
+                    "urdf",
+                    "common_kuka.xacro",
+                ]
+            ),
+            " ",
+            "prefix:=",
+            satellite_2_ns_name,
+            " ",
+            "origin:=",
+            '"3 0 0"',
+            " ",
+            "use_mock_hardware:=",
+            use_mock_hardware,
+            " ",
+            "controllers_file:=kuka_6dof_controllers.yaml",
+            " ",
+            "robot_description_package:=kuka_kr16_support",
+            " ",
+            "robot_description_macro_file:=kr16_2_macro.xacro",
+            " ",
+            "robot_name:=kuka_kr16_2",
+            " ",
+            "listen_ip_address:=",
+            listen_ip_address,
+            " ",
+            "listen_port:=",
+            listen_port,
+            " ",
+        ]
+    )
+    robot_satellite_2_description = {
+        "robot_description": robot_satellite_2_description_content
+    }
+    robot_controllers_satellite_2 = PathJoinSubstitution(
+        [
+            FindPackageShare("two_distributed_kukas_bringup"),
+            "controller_config",
+            "second_distributed_kuka_kr16_2_controller.yaml",
+        ]
+    )
+
+    sub_2_control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        namespace=satellite_2_ns_name,
+        parameters=[robot_satellite_2_description, robot_controllers_satellite_2],
+        remappings=[
+            (
+                "/forward_position_controller/commands",
+                "/position_commands",
+            ),
+            ("/joint_states", slash_satellite_2_ns_name + "/joint_states"),
+            (
+                "/dynamic_joint_states",
+                slash_satellite_2_ns_name + "/dynamic_joint_states",
+            ),
+        ],
+        output="both",
+    )
+
+    robot_state_pub_node_2 = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        namespace=satellite_2_ns_name,
+        output="both",
+        parameters=[robot_satellite_2_description],
+    )
+
+    joint_state_broadcaster_spawner_2 = Node(
+        package="controller_manager",
+        executable="spawner",
+        namespace=satellite_2_ns_name,
+        arguments=[
+            "joint_state_broadcaster",
+            "-c",
+            satellite_2_controller_manager_name,
+        ],
+    )
+
+    robot_controller_spawner_2 = Node(
+        package="controller_manager",
+        executable="spawner",
+        namespace=satellite_2_ns_name,
+        arguments=[
+            "forward_position_controller",
+            "-c",
+            satellite_2_controller_manager_name,
+        ],
+    )
+
+    # RVIZ
+
     rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare("kuka_resources"), "config", "view_robot.rviz"]
+        [
+            FindPackageShare("two_distributed_kukas_bringup"),
+            "rviz_config",
+            "two_distributed_kukas_all_in_on.rviz",
+        ]
     )
 
     rviz_node = Node(
@@ -228,7 +358,7 @@ def generate_launch_description():
 
     delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
         event_handler=OnProcessExit(
-            target_action=joint_state_broadcaster_spawner,
+            target_action=joint_state_broadcaster_spawner_2,
             on_exit=[rviz_node],
         )
     )
@@ -237,8 +367,11 @@ def generate_launch_description():
         control_node,
         robot_state_pub_node,
         joint_state_broadcaster_spawner,
-        delay_rviz_after_joint_state_broadcaster_spawner,
+        sub_2_control_node,
+        robot_state_pub_node_2,
+        joint_state_broadcaster_spawner_2,
         delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
+        delay_rviz_after_joint_state_broadcaster_spawner,
     ]
 
     return LaunchDescription(declared_arguments + nodes)
